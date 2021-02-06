76116074c821d9dd39f341975e5bde780c1dffe0
nana1243 <hieun1243@gmail.com>
2021-02-06 13:46:06 +0900

    fea) pandas.apply함수

---
+++ study/practice_apply.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+
+"""
+
+DataFrame.apply(func, axis=0, raw=False, result_type=None, args=(), **kwds)
+    Apply a function along an axis of the DataFrame.
+    
+    Param:
+        - func : 각 row or column에 행할 연산함수
+        - axis : row or column에 적용할 것 인지
+        
+    Return:
+        - Series or DataFrame
+    
+"""
+
+df = pd.DataFrame([[4, 9]] * 3, columns=['A', 'B'])
+print(df)
+print(df.apply(np.sqrt))
+print(df.apply(np.sum,axis=0))
+print(df.apply(np.sum,axis=1))
