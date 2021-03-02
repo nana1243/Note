6298725d45ec5048f2ffb9416a50c40694b01660
nana1243 <hieun1243@gmail.com>
2021-03-02 17:58:52 +0900

    fea) future객체

--- study/practice_future.py
+++ study/practice_future.py
@@ -1,11 +1,6 @@
 import concurrent.futures
 
 """
-submit : 
- fn 이 fn(*args **kwargs) 처럼 실행되도록 예약하고, 
- 콜러블 객체의 실행을 나타내는 Future 객체를 반환합니다.
-
-Future:
 Future 클래스는 콜러블 객체의 비동기 실행을 캡슐화합니다. Future 인스턴스는 Executor.submit() 에 의해 생성됩니다.
 """
 
@@ -21,4 +16,3 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
 
     for future in concurrent.futures.as_completed(future_dict):
         print(future.result(), future_dict[future])
-
