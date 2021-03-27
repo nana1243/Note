d2ebb537761dbe63b34551a09c6f32412f07d505
nana1243 <hieun1243@gmail.com>
2021-03-27 12:19:07 +0900

    fea) helper class 정의

---
+++ definition/helper_class.py
@@ -0,0 +1,9 @@
+"""
+Definition:
+    In object-oriented programming,
+    a helper class is used to assist in providing some functionality,
+    which isn't the main goal of the application or class
+    in which it is used[citation needed].
+    An instance of a helper class is called a helper object
+    https://en.wikipedia.org/wiki/Helper_class
+"""
